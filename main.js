f265f8ad67600d129fe445d25161b5d84de33c58
syed2008 <78297824+syed2008@users.noreply.github.com>
2021-10-04 20:10:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+user_name = "";
+function sho(){
+    if(age > "18"){
+   document.getElementById("hi").innerHTML = "You are less than 18 years";
+    } else{
+        name = document.getElementById("name").value;
+        document.getElementById("nae").innerHTML = name;
+        grade = document.getElementById("grade").value;
+        document.getElementById("g").innerHTML = grade;
+        city = document.getElementById("city").value;
+        document.getElementById("c").innerHTML = city;
+    }
+
+}
+
